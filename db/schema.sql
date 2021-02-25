b6cbfca2e1046ff130dc11f9aec78ed9eec01b71
Myrmoxenus <scopocira@gmail.com>
2021-02-25 02:10:35 -0600

    Initial upload, schema and server

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+/*Creates or drops and recreates database if exists */
+DROP DATABASE IF EXISTS burgers_db;
+CREATE database burgers_db;
+
+USE burgers_db;
+
+/* Creates table for burgers */
+DROP TABLE IF EXISTS burgers;
+CREATE TABLE burgers (
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    burger_name VARCHAR(30) NOT NULL,
+    devoured BOOLEAN NOT NULL DEFAULT 0
+);
+
+
